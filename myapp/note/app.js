b2aa5b10953d5cfad221f32cf2b6a7b3ddbe385e
李瑛 <liying@liyingdeMacBook-Pro.local>
2017-10-08 15:16:15 +0800

    test eslint

--- myapp/note/app.js
+++ myapp/note/app.js
@@ -1,46 +1,46 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var session = require('express-session');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
 
-var index = require('./routes/index');
-var api = require('./routes/api');
-var auth = require('./routes/auth');
+const index = require('./routes/index');
+const api = require('./routes/api');
+const auth = require('./routes/auth');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'dsfh653gdfjdf'}));
+app.use(session({ secret: 'dsfh653gdfjdf' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', index); //我的便签
-app.use('/api', api); //ajax 接口
-app.use('/auth', auth); //登录
+app.use('/', index); // 我的便签
+app.use('/api', api); // ajax 接口
+app.use('/auth', auth); // 登录
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
